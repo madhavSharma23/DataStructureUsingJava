7d7995b78acd4ab6129f29695b3989a4378e6995
madhavSharma23 <74015807+madhavSharma23@users.noreply.github.com>
2022-06-02 22:47:40 +0530

    Added Comments

--- src/recursion/Factorial.java
+++ src/recursion/Factorial.java
@@ -3,13 +3,17 @@ package recursion;
 import java.util.Scanner;
 
 public class Factorial {
+    //method to return factorial of a given number using recursion
     public static int factRecursive(int n){
+        //base condition
         if(n==0 || n==1){
             return n;
         }
+        //additional condition
         if(n < 0){
             return -1;
         }
+        //recursive leap of faith
         return n*factRecursive(n-1);
     }
     public static void main(String[] args) {
