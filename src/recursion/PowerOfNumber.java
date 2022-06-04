976ae5af26ee526690868bc3749f323209464df8
madhavSharma23 <74015807+madhavSharma23@users.noreply.github.com>
2022-06-04 18:24:55 +0530

    Added Power recursive

--- src/recursion/PowerOfNumber.java
+++ src/recursion/PowerOfNumber.java
@@ -1,4 +1,19 @@
 package recursion;
 
 public class PowerOfNumber {
+    public static int powRecursive(int base,int exponent){
+        if(exponent<0){
+            return -1;
+        }
+        if(exponent==0){
+            return 1;
+        }
+        if(exponent==1){
+            return  base;
+        }
+        return base * powRecursive(base,exponent-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(powRecursive(3,0));
+    }
 }
